89f58f4f6f56baa7c193c3913dffbe90ce801b12
Subcool <peter@subcool>
2019-12-03 10:19:04 +0000

    Add Fake test to all test projects

--- src/ESFA.DC.LARS.Web.Services.Tests/Class1.cs
+++ src/ESFA.DC.LARS.Web.Services.Tests/Class1.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using Xunit;
 
 namespace ESFA.DC.LARS.Web.Services.Tests
 {
     public class Class1
     {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
     }
 }
