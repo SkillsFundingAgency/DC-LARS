73030e7caae7499f66e7ea9a9396e47ff316ce0a
jamie <jamie.coales@education.gov.uk>
2019-12-11 16:05:00 +0000

    swappped out logging

---
+++ src/ESFA.DC.LARS.API.Modules/LoggingModule.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using ESFA.DC.Telemetry;
+using ESFA.DC.Telemetry.Interfaces;
+
+namespace ESFA.DC.LARS.API.Modules
+{
+    public class LoggingModule : Module
+    {
+        protected override void Load(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<ApplicationInsightsTelemetry>().As<ITelemetry>();
+        }
+    }
+}
--- src/ESFA.DC.LARS.API.Services/LearningAimJsonService.cs
+++ src/ESFA.DC.LARS.API.Services/LearningAimJsonService.cs
@@ -3,20 +3,26 @@ using System.Threading.Tasks;
 using ESFA.DC.LARS.API.Interfaces.ReferenceData;
 using ESFA.DC.LARS.API.Interfaces.Services;
 using ESFA.DC.LARS.API.Models;
+using ESFA.DC.Telemetry.Interfaces;
 
 namespace ESFA.DC.LARS.API.Services
 {
     public class LearningAimJsonService : ILearningAimService
     {
+        private readonly ITelemetry _telemetry;
         private readonly ILarsCache _larsCache;
 
-        public LearningAimJsonService(ILarsCache larsCache)
+        public LearningAimJsonService(
+            ITelemetry telemetry,
+            ILarsCache larsCache)
         {
+            _telemetry = telemetry;
             _larsCache = larsCache;
         }
 
         public Task<IEnumerable<LearningAimModel>> GetLearningAims(SearchModel searchParameters)
         {
+            _telemetry.TrackEvent("In Get Learning Aims");
             return _larsCache.GetLearningAims(false);
         }
     }
