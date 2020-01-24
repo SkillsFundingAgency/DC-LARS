553321fb15f492aeb9814f68532b219dfc35b1db
jamie <jamie.coales@education.gov.uk>
2020-01-24 16:08:26 +0000

    learning aim category ui

---
+++ src/ESFA.DC.LARS.Web.Models/ViewModels/LearningAimsCategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.LARS.Web.Models.ViewModels
+{
+    public class LearningAimsCategoryViewModel
+    {
+        public LearningAimModel LearningAimModel { get; set; }
+
+        public IEnumerable<CategoryModel> CategoryModels { get; set; }
+    }
+}
---
+++ src/ESFA.DC.LARS.Web/Controllers/LearningAimCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using ESFA.DC.LARS.Web.Interfaces.Services;
+using ESFA.DC.LARS.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESFA.DC.LARS.Web.Controllers
+{
+    [Route("LearningAimCategories")]
+    public class LearningAimCategoryController : Controller
+    {
+        private readonly ILearningAimsApiService _learningAimsApiService;
+
+        public LearningAimCategoryController(
+            ILearningAimsApiService learningAimsApiService)
+        {
+            _learningAimsApiService = learningAimsApiService;
+        }
+
+        [Route("{learnAimRef}")]
+        public async Task<IActionResult> Index(string learnAimRef)
+        {
+            var viewModel = new LearningAimsCategoryViewModel
+            {
+                LearningAimModel = await _learningAimsApiService.GetLearningAim(learnAimRef)
+            };
+
+            return View(viewModel);
+        }
+    }
+}
