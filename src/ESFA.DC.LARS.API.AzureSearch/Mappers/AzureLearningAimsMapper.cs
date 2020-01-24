6efd8b8e6ab92c91618ba7177c56ddabac1bca3e
jamie <jamie.coales@education.gov.uk>
2020-01-24 15:13:49 +0000

    more tests

--- src/ESFA.DC.LARS.API.AzureSearch/Mappers/AzureLearningAimsMapper.cs
+++ src/ESFA.DC.LARS.API.AzureSearch/Mappers/AzureLearningAimsMapper.cs
@@ -1,22 +1,10 @@
-﻿using System.Linq;
-using ESFA.DC.LARS.API.Interfaces.ReferenceData;
+﻿using ESFA.DC.LARS.API.Interfaces.ReferenceData;
 using ESFA.DC.LARS.Azure.Models;
 
 namespace ESFA.DC.LARS.API.AzureSearch.Mappers
 {
     public class AzureLearningAimsMapper : IMapper<LearningAimModel, Models.LearningAimModel>
     {
-        private readonly IMapper<CategoryModel, Models.CategoryModel> _categoryMapper;
-        private readonly IMapper<FundingModel, Models.FundingModel> _fundingModelMapper;
-
-        public AzureLearningAimsMapper(
-            IMapper<CategoryModel, Models.CategoryModel> categoryMapper,
-            IMapper<FundingModel, Models.FundingModel> fundingModelMapper)
-        {
-            _categoryMapper = categoryMapper;
-            _fundingModelMapper = fundingModelMapper;
-        }
-
         public Models.LearningAimModel Map(LearningAimModel input)
         {
             return new Models.LearningAimModel
@@ -26,11 +14,7 @@ namespace ESFA.DC.LARS.API.AzureSearch.Mappers
                 Type = input.Type,
                 AwardingBody = input.AwardingBody,
                 Level = input.Level,
-                GuidedLearningHours = input.GuidedLearningHours,
-                Level2Category = input.Level2Category,
-                Level3Category = input.Level3Category,
-                Categories = input.Categories.Select(_categoryMapper.Map).ToList(),
-                FundingModels = input.FundingModels.Select(_fundingModelMapper.Map).ToList()
+                GuidedLearningHours = input.GuidedLearningHours
             };
         }
     }
