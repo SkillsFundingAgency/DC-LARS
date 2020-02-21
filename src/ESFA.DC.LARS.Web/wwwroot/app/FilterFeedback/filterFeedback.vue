d34fa6a9e9104803993f324f4de00e0e12abe0c7
jamie <jamie.coales@education.gov.uk>
2020-02-21 08:56:16 +0000

    poc

---
+++ src/ESFA.DC.LARS.Web/wwwroot/app/FilterFeedback/filterFeedback.vue
@@ -0,0 +1,26 @@
+﻿<template>
+    <div class="filter-feedback-container" style="display: block;">
+        <span id="from" class="govuk-body govuk-!-font-size-16">
+            <strong>Showing </strong>
+            <span id="firstFilter">
+                <a href="#" class="filter-feedback" v-for="filter in filters"> 
+                    <span class="filter-name"> 
+                        <span class="close"></span>
+                        {{ filter }}
+                    </span> 
+                </a>
+            </span>
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        el: '#filter-feedback',
+        props: {
+            filters: {
+                type : Object
+            }
+        }
+    }
+</script>
---
+++ src/ESFA.DC.LARS.Web/wwwroot/app/Search.js
@@ -0,0 +1,9 @@
+﻿import Vue from "../assets/dist/minified/vue.min.js";
+import FilterFeedback from "./FilterFeedback/filterFeedback";
+
+var app = new Vue({
+    el: "#app",
+    components: {
+        'filter-feedback': FilterFeedback
+    }
+});
