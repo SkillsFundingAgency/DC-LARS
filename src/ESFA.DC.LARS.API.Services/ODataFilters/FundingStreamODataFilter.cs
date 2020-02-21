03e5b68b7c8109ddec249bacb30d0004a65dbbab
jamie <jamie.coales@education.gov.uk>
2020-02-21 11:07:05 +0000

    fix invalid funding streams showing in search results

--- src/ESFA.DC.LARS.API.Services/ODataFilters/FundingStreamODataFilter.cs
+++ src/ESFA.DC.LARS.API.Services/ODataFilters/FundingStreamODataFilter.cs
@@ -1,15 +1,39 @@
-﻿using ESFA.DC.LARS.API.Interfaces.Services;
+﻿using System.Linq;
+using System.Text;
+using ESFA.DC.LARS.API.Interfaces.Services;
 using ESFA.DC.LARS.API.Models;
 
 namespace ESFA.DC.LARS.API.Services.ODataFilters
 {
-    public class FundingStreamODataFilter : BaseFilter, IODataFilter
+    public class FundingStreamODataFilter : IODataFilter
     {
-        protected override string FilterODataString => "AcademicYears/any(ay:ay/Validities/any(v:v/ValidityCategory eq '{0}'))";
+        private static string FilterODataString => "AcademicYears/any(ay:ay/Validities/any(v:v/ValidityCategory eq '{0}') and ay/AcademicYear eq '{1}')";
 
         public string ApplyFilter(SearchModel searchModel)
         {
-            return ApplyFilter(searchModel.FundingStreams);
+            var searchFilter = searchModel.FundingStreams;
+
+            if (!(searchFilter?.Any() ?? false) || searchFilter.All(string.IsNullOrWhiteSpace))
+            {
+                return string.Empty;
+            }
+
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("(");
+            foreach (var filter in searchFilter)
+            {
+                if (stringBuilder.Length > 1)
+                {
+                    stringBuilder.Append(" or ");
+                }
+
+                stringBuilder.Append(string.Format(FilterODataString, filter, searchModel.TeachingYears.FirstOrDefault()));
+            }
+
+            stringBuilder.Append(")");
+
+            return stringBuilder.ToString();
         }
     }
 }
