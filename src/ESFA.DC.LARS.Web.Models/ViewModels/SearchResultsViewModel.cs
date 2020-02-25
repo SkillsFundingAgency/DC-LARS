55ab5a36dfa54f779446988ef68376e2b200fab4
jamie <jamie.coales@education.gov.uk>
2020-02-25 10:41:52 +0000

    results page

--- src/ESFA.DC.LARS.Web.Models/ViewModels/SearchResultsViewModel.cs
+++ src/ESFA.DC.LARS.Web.Models/ViewModels/SearchResultsViewModel.cs
@@ -4,6 +4,13 @@ namespace ESFA.DC.LARS.Web.Models.ViewModels
 {
     public class SearchResultsViewModel
     {
+        public SearchResultsViewModel()
+        {
+            ValidationErrors = new List<string>();
+        }
+
+        public List<string> ValidationErrors { get; set; }
+
         public SearchModel SearchModel { get; set; }
 
         public LookUpModel LookUpModel { get; set; }
