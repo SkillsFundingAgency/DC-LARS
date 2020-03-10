e5fe220a980cbf7988d16dedbda0adc5e4a71588
Cory-Wynn <CoryWynn1993@gmail.com>
2020-03-10 10:11:52 +0000

    96834 API mapper template

---
+++ src/ESFA.DC.LARS.API.AzureSearch/Mappers/AzureLearningAimFrameworkMapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ESFA.DC.LARS.API.Interfaces;
+using ESFA.DC.LARS.Azure.Models;
+
+namespace ESFA.DC.LARS.API.AzureSearch.Mappers
+{
+    public class AzureLearningAimFrameworkMapper : IMapper<LearningAimFrameworkModel, Models.LearningAimFrameworkModel>
+    {
+        public Models.LearningAimFrameworkModel Map(LearningAimFrameworkModel input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
