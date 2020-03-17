136b7f5929b288b2ec9675b2f7b89cc877f84ec9
jamie <jamie.coales@education.gov.uk>
2020-03-17 13:18:36 +0000

    remove filters via feedback

--- src/ESFA.DC.LARS.Web/wwwroot/app/FilterFeedback/filterFeedback.vue
+++ src/ESFA.DC.LARS.Web/wwwroot/app/FilterFeedback/filterFeedback.vue
@@ -1,10 +1,10 @@
 ﻿<template>
-    <div class="filter-feedback-container" style="display: block;">
+    <div class="filter-feedback-container" style="display: block;" v-if="filters.length > 0">
         <span id="from" class="govuk-body govuk-!-font-size-16">
             <strong>Showing </strong>
             <span id="firstFilter">
                 <template v-for="filter in filters">
-                    <a href="#" class="filter-feedback">
+                    <a href="#" class="filter-feedback" v-on:click="removeFilter(filter)">
                         <span class="filter-name">
                             <span class="close" v-bind:id="filter.key"></span>
                             {{ filter.value }}
@@ -50,30 +50,68 @@
                 });
         }
 
+        removeFilter(filter: IFilterItem): void {
+            const prefix = filter.key.substring(0, 2);
+            filter.key = filter.key.substring(3);
+
+            let filters = this.savedfilters;
+            switch (prefix) {
+                case 'ab':
+                    filters.awardingBodies = this.removeFromArray(filters.awardingBodies, filter);
+                    break;
+                case 'lv':
+                    this.removeFromArray(filters.levels, filter);
+                    break;
+                case 'fs':
+                    this.removeFromArray(filters.fundingStreams, filter);
+                    break;
+                case 'ty':
+                    this.removeFromArray(filters.teachingYears, filter);
+                    break;
+            }
+
+            this.$store.commit('updateFilters', filters);
+        }
+
+        private removeFromArray(filters: Array<IFilterItem>, item: IFilterItem) : Array<IFilterItem> {
+            const found = filters.find(function (filter) {
+                return filter.key === item.key;
+            });
+
+            if (found !== undefined) {
+                const index = filters.indexOf(found);
+                if (index > -1) {
+                    filters.splice(index, 1);
+                }
+            }
+            return filters;
+        }
+
         private refreshFilters() : void {
             const classScope = this;
-
+            this.filters = [];
+            
             if (this.savedfilters.awardingBodies !== undefined) {
                 this.savedfilters.awardingBodies.forEach(function (value) {
-                    classScope.filters.push({key: value.key, value: value.value });
+                    classScope.filters.push({key: 'ab-' + value.key, value: value.value });
                 });
             }
 
             if (this.savedfilters.levels !== undefined) {
                 this.savedfilters.levels.forEach(function (value) {
-                    classScope.filters.push({key: value.key, value: value.value });
+                    classScope.filters.push({key: 'lv-' + value.key, value: value.value });
                 });
             }
 
             if (this.savedfilters.fundingStreams !== undefined) {
                 this.savedfilters.fundingStreams.forEach(function (value) {
-                    classScope.filters.push({key: value.key, value: value.value });
+                    classScope.filters.push({key: 'fs-' + value.key, value: value.value });
                 });
             }
 
             if (this.savedfilters.teachingYears !== undefined) {
                 this.savedfilters.teachingYears.forEach(function (value) {
-                    classScope.filters.push({key: value.key, value: value.value });
+                    classScope.filters.push({key: 'ty-' + value.key, value: value.value });
                 });
             }
         }
