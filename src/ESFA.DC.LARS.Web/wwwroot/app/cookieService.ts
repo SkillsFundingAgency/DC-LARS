6b5367d88ff2408c6d2a8faab0aa7ed18d4ea15e
Cory-Wynn <CoryWynn1993@gmail.com>
2020-03-18 16:27:02 +0000

    97424 - Cookie service

---
+++ src/ESFA.DC.LARS.Web/wwwroot/app/cookieService.ts
@@ -0,0 +1,18 @@
+﻿export default class CookieService {
+    setCookie(name: string, val: string, days: number) {
+        const date = new Date();
+        const value = val;
+
+        date.setTime(date.getTime() + 24 * 60 * 60 * 1000 * days);
+        document.cookie = name + "=" + value + ";path=/;expires=" + date.toUTCString();
+    }
+
+    getCookie(name: string) {
+        const value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+        return value ? value[2] : null;
+    }
+
+    deleteCookie(name: string) {
+        this.setCookie(name, '', -1);
+    }
+}
