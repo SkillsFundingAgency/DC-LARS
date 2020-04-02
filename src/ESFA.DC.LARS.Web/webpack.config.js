05c78cae6f4abdac543cba6eb2b117a1c1c0d895
jamie <jamie.coales@education.gov.uk>
2020-04-02 11:26:57 +0100

    split apps

--- src/ESFA.DC.LARS.Web/webpack.config.js
+++ src/ESFA.DC.LARS.Web/webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
     entry: {
         './assets/dist/js/site': './wwwroot/assets/js/site.js',
         site : ['./wwwroot/assets/scss/search-box.scss', './wwwroot/assets/scss/site.scss'],
-        './assets/dist/js/app': ['./wwwroot/app/homeMain.ts', './wwwroot/app/resultsMain.ts']
+        './assets/dist/js/search': './wwwroot/app/search.ts', 
+        './assets/dist/js/learningAimSearchResults' : './wwwroot/app/learningAimSearchResults.ts'
     },
     // This is where we define the path where Webpack will place
     // a bundled JS file.
