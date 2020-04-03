6a0c700dd6192972c85e56ac1194cc60f9f609d3
Pete Chamley <peter@chamley.name>
2020-04-03 16:10:32 +0100

    Cope with exception and carry on.

--- src/ESFA.DC.LARS.Web/CustomFilters/TelemetryActionFilter.cs
+++ src/ESFA.DC.LARS.Web/CustomFilters/TelemetryActionFilter.cs
@@ -16,13 +16,18 @@ namespace ESFA.DC.LARS.Web.CustomFilters
 
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            var telemetryArgs = new Dictionary<string, string>();
-            foreach (var arg in context.ActionArguments)
+            try
             {
-                telemetryArgs.Add(arg.Key, arg.Value.ToString());
-            }
+                var telemetryArgs = new Dictionary<string, string>();
+                foreach (var arg in context.ActionArguments)
+                {
+                    telemetryArgs.Add(arg.Key, arg.Value.ToString());
+                }
 
-            _telemetryClient.TrackTrace(context.ActionDescriptor.DisplayName, telemetryArgs);
+                _telemetryClient.TrackTrace(context.ActionDescriptor.DisplayName, telemetryArgs);
+            }
+            catch { }
+
             var resultContext = await next();
         }
     }
