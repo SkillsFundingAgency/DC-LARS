c1683e4953419986e1d6b142ddc3974b913920c9
Martin Freshwater <martyfreshwater@hotmail.com>
2020-04-15 11:02:12 +0000

    99192:  Fixed inconsistent casing in function names.

--- src/ESFA.DC.LARS.Web/wwwroot/app/Helpers/resultsDisplayHelper.ts
+++ src/ESFA.DC.LARS.Web/wwwroot/app/Helpers/resultsDisplayHelper.ts
@@ -5,30 +5,30 @@ export class ResultsDisplayHelper {
 	constructor(private resultsContainer: HTMLElement, private resultsCountContainer: HTMLElement, private validationErrorContainer:HTMLElement) {
 	}
 
-	public SetIsLoading(): void {
-		this.SetInnerHtml(this.resultsContainer, "Loading");
+	public setIsLoading(): void {
+		this.setInnerHtml(this.resultsContainer, "Loading");
 	}
 
-	public UpdateForResponse(response: ISearchResults): void {
-		this.SetResults(response.data);
-		this.SetCount(response.count);
-		this.SetValidationErrors(response.validationErrors);
+	public updateForResponse(response: ISearchResults): void {
+		this.setResults(response.data);
+		this.setCount(response.count);
+		this.setValidationErrors(response.validationErrors);
 	}
 
-	public SetResults(html: string): void {
-		this.SetInnerHtml(this.resultsContainer, html);
+	public setResults(html: string): void {
+		this.setInnerHtml(this.resultsContainer, html);
 	}
 
-	public SetCount(count: number): void {
-		this.SetInnerHtml(this.resultsCountContainer, `${count} results`);
+	public setCount(count: number): void {
+		this.setInnerHtml(this.resultsCountContainer, `${count} results`);
 	}
 
-	public SetValidationErrors(errors: Array<string>): void {
+	public setValidationErrors(errors: Array<string>): void {
 		var html = errors.map(v => `<li class="govuk-error-message">${v}</li>`).join();
-		this.SetInnerHtml(this.validationErrorContainer, html);
+		this.setInnerHtml(this.validationErrorContainer, html);
 	}
 
-	private SetInnerHtml(element: HTMLElement, html: string): void {
+	private setInnerHtml(element: HTMLElement, html: string): void {
 		if (element) {
 			element.innerHTML = html;
 		}
