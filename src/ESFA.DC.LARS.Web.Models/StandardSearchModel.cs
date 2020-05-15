93d10c64af3c03019d37039309ceb08e3fd26204
Hari Dupati <dupatihari@gmail.com>
2020-05-15 22:42:06 +0100

    99960 - added  standards search results UI

---
+++ src/ESFA.DC.LARS.Web.Models/StandardSearchModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESFA.DC.LARS.Web.Models
+{
+    public class StandardSearchModel : BaseSearchModel
+    {
+        public StandardSearchModel()
+        {
+            SearchType = LearningType.Standards;
+        }
+
+      public override LearningType? SearchType { get; set; }
+    }
+}
--- src/ESFA.DC.LARS.Web/Controllers/StandardsSearchResultController.cs
+++ src/ESFA.DC.LARS.Web/Controllers/StandardsSearchResultController.cs
@@ -1,15 +1,62 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ESFA.DC.LARS.API.Models;
+using ESFA.DC.LARS.Web.Interfaces;
+using ESFA.DC.LARS.Web.Interfaces.Services;
 using ESFA.DC.LARS.Web.Models;
+using ESFA.DC.LARS.Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using StandardModel = ESFA.DC.LARS.Web.Models.StandardModel;
+using StandardSearchModel = ESFA.DC.LARS.Web.Models.StandardSearchModel;
 
 namespace ESFA.DC.LARS.Web.Controllers
 {
     [Route("StandardsSearchResult")]
-    public class StandardsSearchResultController : Controller
+    public class StandardsSearchResultController : BaseResultsController<StandardSearchModel, StandardModel>
     {
-        public async Task<IActionResult> Index(BasicSearchModel basicSearchModel = null)
+        private const string ResultsTemplate = "_SearchResults";
+
+        private readonly IStandardApiService _standardApiService;
+        private readonly ISearchModelFactory _searchModelFactory;
+        private readonly IClientValidationService _clientValidationService;
+
+        public StandardsSearchResultController(
+            IStandardApiService standardApiService,
+            ISearchModelFactory searchModelFactory,
+            ILookupApiService lookupApiService,
+            IClientValidationService clientValidationService,
+            IEnumerable<ISearchResultsRouteStrategy> resultRouteStrategies)
+            : base(resultRouteStrategies, lookupApiService, ResultsTemplate, LearningType.Standards)
         {
-           return View();
+            _standardApiService = standardApiService;
+            _searchModelFactory = searchModelFactory;
+            _clientValidationService = clientValidationService;
+        }
+
+        [HttpGet("ClearFilters")]
+        public async Task<IActionResult> ClearFilters(string searchTerm)
+        {
+            var model = await PopulateViewModel(null, new StandardSearchModel { SearchTerm = searchTerm, SearchType = _searchType });
+            return View("Index", model);
+        }
+
+        protected override Task<IEnumerable<StandardModel>> GetSearchResults(StandardSearchModel searchModel)
+        {
+            return _standardApiService.GetStandards(searchModel);
+        }
+
+        protected override StandardSearchModel GetSearchModel(BasicSearchModel basicSearchModel)
+        {
+            return _searchModelFactory.GetStandardSearchModel(basicSearchModel);
+        }
+
+        protected override void ValidateSearch(StandardSearchModel searchModel, SearchResultsViewModel<StandardSearchModel, StandardModel> viewModel)
+        {
+            var searchTermError = _clientValidationService.SearchTermLengthValid(searchModel.SearchTerm);
+            if (!string.IsNullOrEmpty(searchTermError))
+            {
+                viewModel.ValidationErrors.Add(searchTermError);
+            }
         }
     }
 }
