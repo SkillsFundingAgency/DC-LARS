9b967c00906896b46250cb00300bab883148c10d
Hari Dupati <dupatihari@gmail.com>
2020-05-19 16:30:34 +0100

    99960- removed newtsoft reference and tostring

--- src/ESFA.DC.LARS.API.Models/ErrorDetails.cs
+++ src/ESFA.DC.LARS.API.Models/ErrorDetails.cs
@@ -10,10 +10,5 @@ namespace ESFA.DC.LARS.API.Models
         public int StatusCode { get; set; }
 
         public string Message { get; set; }
-
-        public override string ToString()
-        {
-            return JsonConvert.SerializeObject(this);
-        }
     }
 }
