1a6645ce7eb455efee892d9e3a52d0047588d0e4
Hari Dupati <dupatihari@gmail.com>
2020-06-01 12:30:06 +0100

    build fix

---
+++ src/ESFA.DC.LARS.Web.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ESFA.DC.LARS.Web.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
