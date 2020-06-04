34ce3d5db592a7ba7ee7be1016fd2a9b617e1ae1
Martin Freshwater <martyfreshwater@hotmail.com>
2020-06-04 15:24:44 +0000

    101299:  Updated to only use required pollyfills.

--- src/ESFA.DC.LARS.Web/wwwroot/pollyfills.js
+++ src/ESFA.DC.LARS.Web/wwwroot/pollyfills.js
@@ -1,1 +1,6 @@
-﻿import 'core-js';
+﻿import 'core-js/features/object';
+import 'core-js/features/array';
+import 'core-js/features/set';
+import 'core-js/features/promise';
+import 'core-js/features/dom-collections';
+import 'core-js/features/url-search-params';
