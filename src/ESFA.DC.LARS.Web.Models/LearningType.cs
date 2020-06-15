182ad12f1c1646b392e247ce83bd1997129be601
Martin Freshwater <martyfreshwater@hotmail.com>
2020-06-15 08:10:34 +0000

    101621:  Updated display text.

--- src/ESFA.DC.LARS.Web.Models/LearningType.cs
+++ src/ESFA.DC.LARS.Web.Models/LearningType.cs
@@ -1,4 +1,6 @@
-﻿namespace ESFA.DC.LARS.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESFA.DC.LARS.Web.Models
 {
     public enum LearningType
     {
@@ -6,6 +8,7 @@
         Frameworks = 1,
         Units = 2,
         Standards = 3,
+        [Display(Name = "T Levels")]
         TLevels = 4
     }
 }
