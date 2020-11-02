d30a55d33102328e0f24134cdd426643e7bd5e21
Martin Freshwater <martyfreshwater@hotmail.com>
2020-11-02 11:49:10 +0000

    104688:  Updated aim display for null values

--- src/ESFA.DC.LARS.Web/Extensions/DateTimeExtensions.cs
+++ src/ESFA.DC.LARS.Web/Extensions/DateTimeExtensions.cs
@@ -4,9 +4,21 @@ namespace ESFA.DC.LARS.Web.Extensions
 {
     public static class DateTimeExtensions
     {
+        private const string DateTimeStringFormat = "dd MMMM yyyy";
+
         public static string ToDateString(this DateTime dateTime)
         {
-            return dateTime.ToString("dd MMMM yyyy");
+            return dateTime.ToString(DateTimeStringFormat);
+        }
+
+        public static string ToDateStringOrDefault(this DateTime? dateTime, string defaultValue)
+        {
+            if (dateTime.HasValue)
+            {
+                return dateTime.Value.ToString(DateTimeStringFormat);
+            }
+
+            return defaultValue;
         }
     }
 }
